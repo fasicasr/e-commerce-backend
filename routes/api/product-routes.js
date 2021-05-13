52903b00218c167eb2c713288c61114e14c2560c
Fasica Robi <fasicasr@gmail.com>
2021-05-13 01:19:33 -0400

    modified update route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,20 +21,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  // try {
-  //   const productData = await Product.findByPk(req.params.id, {
-  //     include: [{ model: Category}, { model: Tag}],
-  //   });
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, { model: Tag}],
+    });
 
-  //   if (!productData) {
-  //     res.status(404).json({ message: "Product not found!" });
-  //     return;
-  //   }
+    if (!productData) {
+      res.status(404).json({ message: "Product not found!" });
+      return;
+    }
 
-  //   res.status(200).json(productData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -119,6 +119,22 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "Product not found!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
